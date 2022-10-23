2b28e67bea8b821ed4964ae88a4ab11187ae1b46
kryptify <xinzhe1219@outlook.com>
2022-10-24 01:39:52 +0900

    Update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Nakamoto Hukudashi</li>
+              <li>&copy; Copyright 2017 Nakamoto Hukuda</li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/" target="_blank">Styleshout</a></li>
            </ul>
 
